a8be8e781acbcc0bc3196865e344072d859078fc
Antor4q <tariquelislam2015@gmail.com>
2024-05-11 20:41:51 +0600

    submite assignment section finished

--- src/pages/AssignmentDetails/AssignmentDetails.jsx
+++ src/pages/AssignmentDetails/AssignmentDetails.jsx
@@ -1,12 +1,41 @@
 
-import { useLoaderData, useParams } from "react-router-dom";
+import { useContext } from "react";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
+import { ConnectAuth } from "../../routes/AuthContext";
+import axios from "axios";
 
 
 const AssignmentDetails = () => {
     const assignId = useParams()
     const assignments  = useLoaderData()
     const assignment = assignments.find(ass => ass._id === assignId.id)
-    const {title, image, marks, dueDate, difficultyLevel, description,userImage,userName} = assignment;
+    const {title, image, marks, dueDate, difficultyLevel, description,userImage,userName,userEmail} = assignment;
+    const {user} = useContext(ConnectAuth)
+    const navigate = useNavigate()
+    const handleAttempted = e => {
+        if(user.email === userEmail){
+            alert("You Don't have access to attempted this assignment")
+        }
+        e.preventDefault()
+        const form = e.target;
+        const fileURL= form.file.value;
+        const note = form.note.value;
+        const status = "Pending"
+        const assTitle = title
+        const email = userEmail
+        const name = userName
+        const mark = marks
+        const attemptedAss = {
+            fileURL,note, status, assTitle, email, name, mark
+        }
+        axios.post(`${import.meta.env.VITE_API_URL}/attempted`,attemptedAss)
+        .then(result => {
+            console.log(result.data)
+            alert("You have Successfully attempted this assignment")
+            navigate("/assignments")
+        })
+        
+    }
     
     return (
         <div className="lg:h-screen lg:max-w-[1440px] mx-auto">
@@ -40,19 +69,21 @@ const AssignmentDetails = () => {
                                     <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                                     </form>
                                     <h2 className="text-xl mt-6 text-black font-semibold">Take This Assignment</h2>
+                                    <form onSubmit={handleAttempted}>
                                     <div className="my-3">
                                         
 
-                                        <input type="text" placeholder="Enter Your File Link" className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+                                        <input type="text" name="file" placeholder="Enter Your File Link" className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
                                     </div>
                                     <div>
                                         
 
-                                        <textarea placeholder="Enter Your Note" className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-4 h-32 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"></textarea>
+                                        <textarea placeholder="Enter Your Note" name="note" className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-4 h-32 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"></textarea>
                                         
                                        
                                     </div>
                                     <input type="submit" className="px-8 mt-3 cursor-pointer py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-300 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600" value="Submit" />
+                                    </form>
                                 </div>
                                 </dialog>
                            
