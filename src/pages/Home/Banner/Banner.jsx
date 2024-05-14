961a241b063e3b1bf88b73615ade65ff225b791b
Antor4q <tariquelislam2015@gmail.com>
2024-05-14 14:42:00 +0600

    slider add on banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -34,7 +34,7 @@ const Banner = () => {
                     <div className="">
                       <h1 className="mb-5 lg:w-2/5 text-5xl font-bold">Join ConnectMates for Enhanced Group Learning</h1>
                       <p className="mb-5 lg:w-1/3">ConnectMates offers a dynamic platform for collaborative study. Join now to create assignments, mark them, and engage with peers effortlessly!</p>
-                      <button className="btn btn-primary">Get Started</button>
+                      <button className="btn bg-gradient-to-r from-[#0d434a] via-[#117c8a] to-[#18a6b9]">Get Started</button>
                     </div>
                   </div>
                  </div>
@@ -46,7 +46,7 @@ const Banner = () => {
                     <div className="">
                       <h1 className="mb-5 lg:w-2/5 text-5xl font-bold">Elevate Your Learning Experience with ConnectMates</h1>
                       <p className="mb-5 lg:w-1/3">Discover a new way of learning with ConnectMates. Connect with fellow students, create assignments together, and track your progress seamlessly!</p>
-                      <button className="btn btn-primary">Get Started</button>
+                      <button className="btn bg-gradient-to-r from-[#0d434a] via-[#117c8a] to-[#18a6b9]">Get Started</button>
                     </div>
                   </div>
                  </div>
@@ -58,7 +58,7 @@ const Banner = () => {
                     <div className="">
                       <h1 className="mb-5 lg:w-2/5 text-5xl font-bold">Transform Group Study with ConnectMates</h1>
                       <p className="mb-5 lg:w-1/3 lg:1/3">Take your group study sessions to the next level with ConnectMates. Collaborate on assignments, share knowledge, and achieve academic success together!</p>
-                      <button className="btn btn-primary">Get Started</button>
+                      <button className="btn bg-gradient-to-r from-[#0d434a] via-[#117c8a] to-[#18a6b9]">Get Started</button>
                     </div>
                   </div>
                  </div>
